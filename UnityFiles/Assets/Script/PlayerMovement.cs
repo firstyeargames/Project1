dcd00cda2a726f6bbf911fb3397a17e68238c566
Abdullah <abdul07094@gmail.com>
2019-03-12 19:08:19 +0000

    movemetnt

---
+++ UnityFiles/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    float speed;
+    float xDirection;
+    private bool rightBtn, leftBtn; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rightBtn = false;
+        leftBtn = false;
+    }
+
+    public void moveRight() {
+        rightBtn = true;
+    }
+
+    public void moveLeft()
+    {
+        leftBtn = true;
+        Debug.Log("click");
+    }
+
+    void Update()
+    {
+        if(rightBtn == true)
+        {
+            rb.velocity = Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (leftBtn == true)
+        {
+            rb.velocity = Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}
