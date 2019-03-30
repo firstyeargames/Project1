378069409044909da131aba6ccf55e1fdc2ccf78
Abdullah <abdul07094@gmail.com>
2019-03-30 14:15:17 +0000

    Knife prefab
    Knife prefab added to the scene

---
+++ UnityFiles/Assets/Script/SpawnObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject obj;
+    Vector3 objPosition;
+    Rigidbody objRigidbody;
+    float maxSpawnRange, minSpawnRange, randomSpawns;
+    public List<GameObject> spawnList = new List<GameObject>();
+
+
+    private void Start()
+    {
+        maxSpawnRange = 3.5f;
+        minSpawnRange = -3.5f;
+        // Spawn object every 2 seconds, 1 second apart
+        InvokeRepeating("spawnObj", 2f, 1f);
+        objRigidbody = obj.GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+        // Random range for spawning but within camera screen
+        randomSpawns = Random.Range(minSpawnRange, maxSpawnRange);
+        objPosition = new Vector3(randomSpawns, 15, 15);
+    }
+
+    void spawnObj()
+    {
+        GameObject spawnHolder;
+        spawnHolder = Instantiate(obj, objPosition, Quaternion.identity);
+        objRigidbody.velocity = new Vector3(0, Random.Range(1, 5), 0);
+        spawnList.Add(spawnHolder);
+    }
+}
